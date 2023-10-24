e2f62caae40157913e07ad64be7fa96ba5fa73fd
Adilet <adiletto.kg@gmail.com>
2023-10-23 21:16:59 -0500

    complete markup table/pagination

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,16 +1,80 @@
 import React, { useState } from "react";
 import styles from "./Table.module.scss";
-import { CaretDownOutlined } from "@ant-design/icons";
-import { Pagination } from "antd";
+import {
+  CaretDownOutlined,
+  LeftOutlined,
+  RightOutlined,
+} from "@ant-design/icons";
+import dotsIcon from "../../Assets/admin/admin/DotsThreeVertical.svg"
+
+const staticData = [
+  {
+    id: "№1",
+    name: "Капучино",
+    category: "Кофе",
+    ingredients: "Молоко (70 мл), Кофе (15 зерен), Во...",
+    price: "Молоко (70 мл), Кофе (15 зерен), Во...",
+    branch: "№NeoCafe Dzerzhinka",
+  },
+  {
+    id: "№2",
+    name: "Latte",
+    category: "Кофе",
+    ingredients: "Молоко (70 мл), Кофе (15 зерен), Во...",
+    price: "Молоко (70 мл), Кофе (15 зерен), Во...",
+    branch: "№NeoCafe Dzerzhinka",
+  },
+  {
+    id: "№3",
+    name: "Americano",
+    category: "Кофе",
+    ingredients: "Молоко (70 мл), Кофе (15 зерен), Во...",
+    price: "Молоко (70 мл), Кофе (15 зерен), Во...",
+    branch: "№NeoCafe Dzerzhinka",
+  },
+  {
+    id: "№4",
+    name: "Starbucks",
+    category: "Кофе",
+    ingredients: "Молоко (70 мл), Кофе (15 зерен), Во...",
+    price: "Молоко (70 мл), Кофе (15 зерен), Во...",
+    branch: "№NeoCafe Dzerzhinka",
+  },
+
+  {
+    id: "№5",
+    name: "McCoffee",
+    category: "Кофе",
+    ingredients: "Молоко (70 мл), Кофе (15 зерен), Во...",
+    price: "Молоко (70 мл), Кофе (15 зерен), Во...",
+    branch: "№NeoCafe Dzerzhinka",
+  },
+];
 
 const Table = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [pageSize, setPageSize] = useState(2);
+  const itemsPerPage = 4; // Set the number of items per page
 
-  const handlePageChange = (page, pageSize) => {
-    setCurrentPage(page);
-    setPageSize(pageSize);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = staticData.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handlePaginationClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
+
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (currentPage < Math.ceil(staticData.length/ itemsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   return (
     <div>
       <table className={styles.table}>
@@ -19,7 +83,7 @@ const Table = () => {
             <th>№</th>
             <th>Наименование</th>
             <th className={styles.table__categoryTh}>
-              Категория <CaretDownOutlined />
+              Категория <CaretDownOutlined className={styles.table__caretDown}/>
             </th>
             <th>Состав блюда и граммовка</th>
             <th>Стоимость</th>
@@ -29,50 +93,37 @@ const Table = () => {
           </tr>
         </thead>
         <td colSpan="6">
-          <div className={styles.table__hrLine}></div>
-        </td>
+              <div className={styles.table__hrLine}></div>
+            </td>
         <tbody>
-          <tr>
-            <td>№1</td>
-            <td>Капучино</td>
-            <td>Кофе</td>
-            <td>Молоко (70 мл), Кофе (15 зерен), Во...</td>
-            <td>Молоко (70 мл), Кофе (15 зерен), Во...</td>
-            <td>№NeoCafe Dzerzhinka</td>
-          </tr>
-          <tr>
-            <td>№1</td>
-            <td>Капучино</td>
-            <td>Кофе</td>
-            <td>Молоко (70 мл), Кофе (15 зерен), Во...</td>
-            <td>Молоко (70 мл), Кофе (15 зерен), Во...</td>
-            <td>№NeoCafe Dzerzhinka</td>
-          </tr>
-          <tr>
-            <td>№1</td>
-            <td>Капучино</td>
-            <td>Кофе</td>
-            <td>Молоко (70 мл), Кофе (15 зерен), Во...</td>
-            <td>Молоко (70 мл), Кофе (15 зерен), Во...</td>
-            <td>№NeoCafe Dzerzhinka</td>
-          </tr>
-          <tr>
-            <td>№1</td>
-            <td>Капучино</td>
-            <td>Кофе</td>
-            <td>Молоко (70 мл), Кофе (15 зерен), Во...</td>
-            <td>Молоко (70 мл), Кофе (15 зерен), Во...</td>
-            <td>№NeoCafe Dzerzhinka</td>
-          </tr>
+          {currentItems.map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.category}</td>
+              <td>{item.ingredients}</td>
+              <td>{item.price}</td>
+              <td className={styles.table__branch}>{item.branch} 
+              <img src={dotsIcon} alt="dots" />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <div className={styles.paginationContainer}>
-        <Pagination
-          current={currentPage}
-          pageSize={pageSize}
-          total={10}
-          onChange={handlePageChange}
-        />
+        <button className={styles.leftBtn} onClick={handlePrevClick}>
+          <LeftOutlined />
+        </button>
+        {Array.from({
+          length: Math.ceil(staticData.length / itemsPerPage),
+        }).map((item, index) => (
+          <button className={ index + 1 === currentPage ?  styles.activeNum : undefined} key={index} onClick={() => handlePaginationClick(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+        <button className={styles.rightBtn} onClick={handleNextClick}>
+          <RightOutlined />
+        </button>
       </div>
     </div>
   );
